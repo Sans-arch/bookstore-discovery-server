2fcba7818cd59b3420a0e90e201d78b98598f91c
Santiago Negreira <sansfleitas@gmail.com>
2025-01-18 21:18:21 -0300

    Initialize and config project

---
+++ src/test/java/com/sansarch/bookstore_discovery_server/BookstoreDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansarch.bookstore_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
